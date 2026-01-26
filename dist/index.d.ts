b492bd8124cff53cb30d21ae34b9b6b472dd3815
Marko <markofajler@gmail.com>
2026-01-26 21:52:15 +0100

    Remove node_modules and dist from tracking

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { LocalModelRunner } from "./runner.js";
-export { discoverModels, isOllamaRunning } from "./ollama.js";
-export { getApiKey, setApiKey, clearApiKey, getApiBaseUrl, setApiBaseUrl, getEnvironment, setEnvironment, type Environment, } from "./config.js";
-export { verifyApiKey, pollForRequest, submitProgress, submitResult, } from "./api.js";
-//# sourceMappingURL=index.d.ts.map
